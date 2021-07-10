9b8281d4b31fce9418236e1070324489568a23d5
mong_head <camara1melon315@gmail.com>
2021-07-10 21:00:45 +0900

    game 완성 : 승자 결정

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -4,16 +4,11 @@ import "./index.css";
 
 // function Component : render만 가짐(state 못 씀)
 function Square(props) {
-
-    return (
-      <button
-        className="square"
-        onClick={props.onClick}
-      >
-        {props.value}
-      </button>
-    );
-
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -22,14 +17,18 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true, // 순서
     };
   }
 
-  handleClick(i){
-      // 불변성
-      const squares = this.state.squares.slice(); // squares복사본
-      squares[i] = 'X';
-      this.setState({squares: squares});
+  handleClick(i) {
+    // 불변성
+    const squares = this.state.squares.slice(); // squares복사본
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -42,7 +41,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+	  const winner = calculateWinner(this.state.squares);
+    let status;
+	if(winner){
+		status = 'Winner: ' + winner;
+	} else {
+		status = "Next player: "+ (this.state.xIsNext ? 'X' : 'O');
+	}
 
     return (
       <div>
@@ -86,3 +91,23 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
+
+function calculateWinner(squares) {
+	const lines = [
+	  [0, 1, 2],
+	  [3, 4, 5],
+	  [6, 7, 8],
+	  [0, 3, 6],
+	  [1, 4, 7],
+	  [2, 5, 8],
+	  [0, 4, 8],
+	  [2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+	  const [a, b, c] = lines[i];
+	  if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+		return squares[a];
+	  }
+	}
+	return null;
+  }
